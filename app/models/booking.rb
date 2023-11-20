ee1723683960f71e1aae972475a41484a80b3244
Jerome PRINCET <jerome@princet.fr>
2023-11-20 16:48:14 +0100

    Add controllers & validation sur les modeles

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vehicule
+
+  validates :user_id, presence: true
+  validates :vehicule_id, presence: true
+  validates :date_debut, presence: true
+  validates :date_fin, presence: true, comparison: { :greater_than_or_equal_to :date_debut }
+  validates :total_prix, presence: true, comparison: { greater_than: 0 }
+
 end
--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -1,3 +1,6 @@
 class Vehicule < ApplicationRecord
   belongs_to :user
+
+  has_many :bookings
+  validates :user_id, presence: true
 end
