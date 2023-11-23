c1e2eaf24a8e6f28307e92ed4644c909c6729468
Jeremy Kwan Chung <jeremykwanchung.1995@gmail.com>
2023-11-23 18:21:29 +0100

    category

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,14 @@ file = URI.open("app/assets/images/coccinelle_char.jpg")
 car1.photo.attach(io: file, filename: "car.jpeg", content_type: "image/jpg")
 car1.save
 
-car2 = Vehicule.new(category: "Champignions", model: "Amsterdam", description: "doté d'un cendrier marocain", prix: 130)
+car2 = Vehicule.new(category: "Funny", model: "Amsterdam", description: "doté d'un cendrier marocain", prix: 130)
 car2.user = user
 car2.address = "60 avenue des champs elysées Paris"
 file = URI.open("app/assets/images/photo_vehicule/amanite.jpg")
 car2.photo.attach(io: file, filename: "car.jpeg", content_type: "image/jpg")
 car2.save
 
-car3 = Vehicule.new(category: "Mini", model: "Hot-dog", description: "Voiture special pour les policiers retraités", prix: 135)
+car3 = Vehicule.new(category: "Plat", model: "Hot-dog", description: "Voiture special pour les policiers retraités", prix: 135)
 car3.user = user
 car3.address = "11 rue charles tillon les mureaux"
 file = URI.open("app/assets/images/photo_vehicule/mini_saucisse.jpeg")
@@ -42,7 +42,7 @@ file = URI.open("app/assets/images/photo_vehicule/moto_drole.jpeg")
 car4.photo.attach(io: file, filename: "car.jpeg", content_type: "image/jpeg")
 car4.save
 
-car5 = Vehicule.new(category: "Mp3", model: "No-Shame", description: "slip offert", prix: 155)
+car5 = Vehicule.new(category: "Moto", model: "No-Shame", description: "slip offert", prix: 155)
 car5.user = user
 car5.address = "43 rue de la vanne 92120 Montrouge"
 file = URI.open("app/assets/images/photo_vehicule/MP3_750.jpg")
@@ -50,21 +50,21 @@ car5.photo.attach(io: file, filename: "car.jpeg", content_type: "image/jpg")
 
 car5.save
 
-car6 = Vehicule.new(category: "Car", model: "OmarCar", description: "MANGEZ DES OMARS DANS LA OMAR CARS, CONDUIT PAR OMAR !", prix: 160)
+car6 = Vehicule.new(category: "Plat", model: "OmarCar", description: "Mangez des omars dans la omar cars, conduit par Omar!", prix: 160)
 car6.user = user
 car6.address = "249 avenue du marechal juin boulogne billancourt"
 file = URI.open("app/assets/images/photo_vehicule/omar_sy_car.jpeg")
 car6.photo.attach(io: file, filename: "car.jpeg", content_type: "image/jpeg")
 car6.save
 
-car7 = Vehicule.new(category: "On sait pas", model: "voiture boule", description: "Les tonneaux vont etre interminable", prix: 180)
+car7 = Vehicule.new(category: "Funny", model: "voiture boule", description: "Les tonneaux vont etre interminable", prix: 180)
 car7.user = user
 car7.address = "1 rue de la paix Paris"
 file = URI.open("app/assets/images/photo_vehicule/smart.jpg")
 car7.photo.attach(io: file, filename: "car.jpeg", content_type: "image/jpg")
 car7.save
 
-car8 = Vehicule.new(category: "car", model: "Cardinau", description: "marche au silex pendant 1km", prix: 200)
+car8 = Vehicule.new(category: "Funny", model: "Cardinau", description: "marche au silex pendant 1km", prix: 200)
 car8.user = user
 car8.address = "2 boulevard richard wallace 92800 puteaux"
 file = URI.open("app/assets/images/photo_vehicule/voiture_dinau.jpeg")
