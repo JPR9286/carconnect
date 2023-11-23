e9ae821096dfe90ca98b8ce2bdefe31b19151ce7
Jerome PRINCET <jerome@princet.fr>
2023-11-23 13:59:40 +0100

    Map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_105730) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_123409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,9 +76,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_105730) do
     t.string "model"
     t.string "description"
     t.float "prix"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_vehicules_on_user_id"
   end
 
