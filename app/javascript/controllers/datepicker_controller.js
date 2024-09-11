91aaade90e03d070d1925928b8de80b296937b84
JPRINCET <jerome.princet@powercharge.tech>
2024-09-11 19:17:13 +0200

    test

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,7 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
-
+import flatpickr from "flatpickr";
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  // retrieve the targets from the DOM
+  static targets = [ 'dateDebutInput', 'dateFinInput' ]
+
+  // On controller's connection, call the flatpickr
+  // function in order to build the calendars
   connect() {
+    console.log("hello")
+
+    flatpickr(this.element, {
+      mode: "range",
+      minDate: "today",
+      inline: true,
+      dateFormat: "Y-m-d"
+    });
+
   }
+
 }
